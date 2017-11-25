91edcd829e9dc55c5fcca8c1b7c3edbd1a7748b3
aBaO <bxh883@qq.com>
2017-11-26 00:30:48 +0800

    添加主菜单

---
+++ Assets/MainScene/BackButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+ [RequireComponent(typeof(Button))]
+public class BackButton : MonoBehaviour {
+
+	private Button backButton;
+
+	void Start () {
+		backButton = gameObject.GetComponent<Button>();			
+		backButton.onClick.AddListener(()=> SceneManager.LoadSceneAsync("MainMenu"));
+	}
+	
+}
---
+++ Assets/MainScene/MainMenu.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainMenu : MonoBehaviour {
+
+	public ScrollRect scrollRect;
+	public GameObject buttonTemplete;
+
+	private static Dictionary<string,string> buttonToSceneDictionary = new Dictionary<string, string>(){
+		{"BlurTexture", "BlurTexture"},
+		{"BlurTexture_性能测试", "BlurTexture_PerformanceTest"},
+	};
+
+	void Start () {
+		scrollRect.gameObject.SetActive(false);
+		buttonTemplete.SetActive(false);
+		foreach(var button in buttonToSceneDictionary){
+			GameObject buttonObj = Instantiate(buttonTemplete, buttonTemplete.transform.parent) as GameObject;	
+			buttonObj.name = button.Value;
+			buttonObj.GetComponentInChildren<Text>().text = button.Key;
+			buttonObj.SetActive(true);
+			var copy = button;
+			buttonObj.GetComponent<Button>().onClick.AddListener( ()=> {
+				Debug.Log(string.Format("click [{0}] go to sence [{1}]", copy.Key, copy.Value));	
+				SceneManager.LoadScene(copy.Value);
+			} ); 
+		}	
+		scrollRect.gameObject.SetActive(true);
+	}
+
+	/// <summary>
+	/// 这里的结果并非预期的，因为Mono编译的差别导致 
+	/// </summary>
+	/// <returns>The button to scene.</returns>
+	IEnumerator InitButtonToScene(){
+		buttonTemplete.SetActive(false);
+		foreach(var button in buttonToSceneDictionary){
+			GameObject buttonObj = Instantiate(buttonTemplete, buttonTemplete.transform.parent) as GameObject;	
+			buttonObj.name = button.Key;
+			buttonObj.GetComponentInChildren<Text>().text = button.Key;
+			buttonObj.SetActive(true);
+			var copy = button;
+			buttonObj.GetComponent<Button>().onClick.AddListener( ()=> {
+				Debug.Log(copy);	
+			} ); 
+			yield return 0;
+		}	
+	}
+}
