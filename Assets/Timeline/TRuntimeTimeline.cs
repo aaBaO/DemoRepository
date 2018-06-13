968a3a8d82a4fcece8aa0b9f12dcb2774a83b0d8
aBaO <bxh883@qq.com>
2018-06-13 20:14:06 +0800

    Add test code to create TimelineClip at runtime

---
+++ Assets/Timeline/TRuntimeTimeline.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.Timeline;
+using UnityEngine.Playables;
+using UnityEngine;
+
+public class TRuntimeTimeline : MonoBehaviour {
+
+    public TimelineAsset timelineAsset;
+    private PlayableDirector m_PlayableDirector;
+
+	// Use this for initialization
+	void Start () {
+        timelineAsset = new TimelineAsset();
+
+        LightControlTrack createTrack = timelineAsset.CreateTrack<LightControlTrack>(null, "TestLightControlTrack");
+        TimelineClip createClip = createTrack.CreateClip<LightClip>();
+        LightClip lightClip = createClip.asset as LightClip;
+        LightData lightData = lightClip.templete;
+        lightData.color = Color.yellow; 
+
+        m_PlayableDirector = gameObject.AddComponent<PlayableDirector>();
+        m_PlayableDirector.playableAsset = timelineAsset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
